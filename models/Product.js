47d98e82742a4182f206e7d28addefd3057ff263
Khushal Bairwa <bairwakhushal@gmail.com>
2025-07-06 02:08:28 +0530

    Pagination and middleware added for token

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,11 @@ const ProductSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	price: { type: Number, required: true },
 	description: { type: String, required: true },
+	category: {
+		type: String,
+		required: true,
+		enum: ["laptop", "mobile", "accessory", "tv", "other"],
+	},
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
